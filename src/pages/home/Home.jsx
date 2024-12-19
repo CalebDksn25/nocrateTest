2c2f690399aadfa602b0f0fd90ff5fc59e8dee89
caleb dickson <caleb.n.dickson@gmail.com>
2024-12-19 10:03:59 -0800

    frontend updates

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,39 +5,41 @@ import "./home.css";
 function Home() {
   return (
     <div className="container same-bg">
-      <h1 className="title">Nocrate.</h1>
-      <ul className="menu">
-        <li>
-          <span>00</span> -{" "}
-          <Link to="/" className="link-text">
-            HOME
-          </Link>
-        </li>
-        <li>
-          <span>01</span> -{" "}
-          <Link to="/login" className="link-text">
-            Account
-          </Link>
-        </li>
-        <li>
-          <span>02</span> -{" "}
-          <Link to="/shop" className="link-text">
-            SHOP ALL
-          </Link>
-        </li>
-        <li>
-          <span>03</span> -{" "}
-          <Link to="/catalogue" className="link-text">
-            CATALOGUE
-          </Link>
-        </li>
-        <li>
-          <span>04</span> -{" "}
-          <Link to="/contact" className="link-text">
-            CONTACT
-          </Link>
-        </li>
-      </ul>
+      <div className="center-content">
+        <h1 className="title">Nocrate.</h1>
+        <ul className="menu">
+          <li>
+            <span>00</span> -{" "}
+            <Link to="/" className="link-text">
+              HOME
+            </Link>
+          </li>
+          <li>
+            <span>01</span> -{" "}
+            <Link to="/login" className="link-text">
+              Account
+            </Link>
+          </li>
+          <li>
+            <span>02</span> -{" "}
+            <Link to="/shop" className="link-text">
+              SHOP ALL
+            </Link>
+          </li>
+          <li>
+            <span>03</span> -{" "}
+            <Link to="/catalogue" className="link-text">
+              CATALOGUE
+            </Link>
+          </li>
+          <li>
+            <span>04</span> -{" "}
+            <Link to="/contact" className="link-text">
+              CONTACT
+            </Link>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
