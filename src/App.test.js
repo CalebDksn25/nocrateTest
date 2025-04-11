8bf156ae9c64ef6263b96ceef2a8ad2f9c24344b
Caleb Dickson <157729906+CalebDksn25@users.noreply.github.com>
2025-04-11 12:50:51 -0700

    build updates

--- src/App.test.js
+++ src/App.test.js
@@ -1,74 +1,8 @@
-import React, { useState } from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  useLocation,
-  Navigate,
-} from "react-router-dom";
-import "./App.css";
-import Home from "./pages/home/Home";
-import Shop from "./pages/shop/Shop";
-import Catalogue from "./pages/catalogue/Catalogue";
-import Contact from "./pages/contact/Contact";
-import Header from "./components/header/Header";
-import Product from "./pages/product/Product";
-import Login from "./pages/login/Login";
-import Signup from "./pages/signup/Signup";
-import Cart from "./pages/cart/Cart"; // Import Cart component
-import CartSidebar from "./components/cartSidebar/CartSidebar"; // Import CartSidebar component
-import { CartProvider } from "./utils/CartContext"; // Import CartProvider
-import Password from "./pages/password/Password";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-function AppContent() {
-  const location = useLocation();
-  const isHomePage = location.pathname === "/";
-  const isPasswordPage = location.pathname === "/password";
-  const [isCartSidebarOpen, setIsCartSidebarOpen] = useState(false);
-  const [isPasswordProtected, setIsPasswordProtected] = useState(false); // Set to true to enable password protection
-
-  // If password protection is enabled and user is not on password page, redirect to password page
-  if (isPasswordProtected && !isPasswordPage) {
-    return <Navigate to="/password" replace />;
-  }
-
-  return (
-    <div className="App">
-      {!isHomePage && !isPasswordPage && <Header />}
-      <CartSidebar
-        isOpen={isCartSidebarOpen}
-        onClose={() => setIsCartSidebarOpen(false)}
-      />
-      <div className="main-content">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/shop" element={<Shop />} />
-          <Route path="/catalogue" element={<Catalogue />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/product/:productId" element={<Product />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route
-            path="/password"
-            element={
-              <Password setIsPasswordProtected={setIsPasswordProtected} />
-            }
-          />
-        </Routes>
-      </div>
-    </div>
-  );
-}
-
-function App() {
-  return (
-    <CartProvider>
-      <Router>
-        <AppContent />
-      </Router>
-    </CartProvider>
-  );
-}
-
-export default App;
+test("renders learn react link", () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
